7bb4085a228561511cdcf06b8a360f5d70b1f517
Munir Sharif <munirsharif900@gmail.com>
2026-02-10 17:48:01 +0500

    login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -69,7 +69,7 @@ export default function Login() {
 
     return (
         <div className="grid md:grid-cols-2 items-center  h-full overflow-hidden ">
-            <div className="relative order-1 min-h-150 md:min-h-screen">
+            {/* <div className="relative order-1 min-h-150 md:min-h-screen">
                 <img
                     src={LogoImage}
                     alt="login-image"
@@ -79,7 +79,7 @@ export default function Login() {
                     fetchpriority="high"
                     loading="eager"
                 />
-            </div>
+            </div> */}
             <div className="relative order-1 min-h-150 md:min-h-screen">
                 <img
                     src={LogoImage}
@@ -97,9 +97,9 @@ export default function Login() {
                 <main className="max-w-125 mx-auto px-4 flex flex-col justify-center h-full">
                     <div className="h-fit border p-5 border-[#E2E8F0] rounded-xl shadow">
                         <div className="mb-6 text-center">
-                            <div className="flex justify-center  mb-6">
+                            {/* <div className="flex justify-center  mb-6">
                                 <img src={HeaderLogo} alt="logo" width={200} height={45} style={{ width: "200px", height: "45px" }} />
-                            </div>
+                            </div> */}
                             <div className="flex justify-center mb-6">
                                 <img
                                     src={HeaderLogo}
